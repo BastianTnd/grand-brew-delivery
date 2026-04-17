d793f5255ced406e5c307fc065f5d5b5ddaff918
Nalin Caswell <3005105@stud.hs-mannheim.de>
2026-04-17 16:08:53 +0200

    feature car routine: - car now moves in a routine, but still little but buggy - collision with car routine works (more ro less)

--- godot_project/NPCs/CarNPCs/car_npc.gd
+++ godot_project/NPCs/CarNPCs/car_npc.gd
@@ -1,8 +1,20 @@
 extends CharacterBody2D
 
+var crash_cooldown = 0.0
+@export var cooldown_time = 0.8
 
-func _physics_process(_delta: float) -> void:
+func _physics_process(delta: float) -> void:
+	if crash_cooldown > 0:
+		crash_cooldown -= delta
+
+	# Take position from PathFollow2D
+	var path_follow = get_parent() # PathFollow2D
+	global_position = path_follow.global_position
+	rotation = path_follow.rotation
+
+	# move_and_slide für Collision Detection aufrufen
 	var collided = move_and_slide()
-	
-	if collided:
-		print("Crashed")
+
+	if collided and crash_cooldown <= 0:
+		crash_cooldown = cooldown_time
+		print("NPC Crashed!")
