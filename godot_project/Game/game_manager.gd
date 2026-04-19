e534fd3260078148e5083378a3a8ec533ba13f7b
Nalin Caswell <3005105@stud.hs-mannheim.de>
2026-04-19 13:11:45 +0200

    Game Manager Script: - now 5 spwan points that rotate through

--- godot_project/Game/game_manager.gd
+++ godot_project/Game/game_manager.gd
@@ -1,6 +1,13 @@
 extends Node2D
 
 var collectible_blueprint = preload("res://Collectible/collectible.tscn")
+var spawn_points = {
+	1: [Vector2(350, 195), Vector2(730, 245), Vector2(-880, 1275)],
+	2: [Vector2(760, 1320), Vector2(-540, 880), Vector2(950, -70)],
+	3: [Vector2(-850, 480), Vector2(200, 1030), Vector2(820, 950)],
+	4: [Vector2(950, 1350), Vector2(-680, 600), Vector2(350, 340)],
+	5: [Vector2(603, 580), Vector2(-535, 1360), Vector2(-5, 690)],
+}
 
 func _ready():
 	spawn_items()
@@ -13,9 +20,14 @@ func _input(_event: InputEvent) -> void:
 		get_tree().change_scene_to_file("res://Game/PauseMenu/pause_menu.tscn")
 
 func spawn_items():
+	var random_number_spawn_points = randi_range(1, 5)
+	var collectible_spawn_points = spawn_points[random_number_spawn_points]
+	print("Spawn Punkte: ", collectible_spawn_points)
+	
 	var types = [Collectible.collectible_type.MALT, Collectible.collectible_type.HOPS, Collectible.collectible_type.YEAST]
-	for t in types:
+	
+	for index in range(types.size()):
 		var item = collectible_blueprint.instantiate()
-		item.collectible = t
+		item.collectible = types[index]
 		add_child(item)
-		item.position = Vector2(randf_range(200, 1600), randf_range(200, 1000))
+		item.position = collectible_spawn_points[index]
