da280eb978aa2c3fcaa25bcb4c18527145c5cb50
Nalin Caswell <3005105@stud.hs-mannheim.de>
2026-04-19 18:55:36 +0200

    Main Menu / Credits Scene: - added new Credits Window

--- godot_project/Game/MainMenu/main_menu.gd
+++ godot_project/Game/MainMenu/main_menu.gd
@@ -8,6 +8,9 @@ func _on_play_pressed() -> void:
 func _on_how_to_play_pressed() -> void:
 	get_tree().change_scene_to_file("res://Game/HowToPlayMenu/how_to_play.tscn")
 	
+func _on_credits_pressed() -> void:
+	get_tree().change_scene_to_file("res://Game/CreditsMenu/credits.tscn")
+	
 
 func _on_quit_pressed() -> void:
 	get_tree().quit()
