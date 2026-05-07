1d4aa50a5df16c50d2c7c779fd009bf3330b5412
Bastian Triendl <3001962@stud.th-mannheim.de>
2026-05-07 02:59:26 +0200

    feature(score_manager):
    - added local score-file as JSON

--- godot_project/score_manager.gd
+++ godot_project/score_manager.gd
@@ -14,8 +14,12 @@ var has_yeast : bool = false
 
 var is_game_active : bool = false
 
+var highscore_file_path = "user://highscores.json"
+var highscores = []
+
 func _ready():
 	time_left = game_time
+	load_highscores()
 
 func _process(delta):
 	if is_game_active and time_left > 0:
@@ -48,6 +52,7 @@ func complete_delivery():
 
 func _trigger_end():
 	is_game_active = false
+	save_new_score(int(round(total_points)))
 	get_tree().change_scene_to_file("res://Game/GameOverScreen/GameOverScreen.tscn")
 
 func reset_game():
@@ -58,3 +63,38 @@ func reset_game():
 	has_yeast = false
 	time_left = game_time
 	is_game_active = false
+
+
+func save_new_score(score_value: int):
+	var new_entry = {"score": score_value, "date": Time.get_date_string_from_system()}
+	highscores.append(new_entry)
+	
+	highscores.sort_custom(func(a, b): return a["score"] > b["score"])
+	
+	if highscores.size() > 5:
+		highscores.resize(5)
+	
+	var file = FileAccess.open(highscore_file_path, FileAccess.WRITE)
+	if file:
+		var json_string = JSON.stringify(highscores)
+		file.store_string(json_string)
+		file.close()
+		print("Score lokal gespeichert!")
+
+func load_highscores():
+	if not FileAccess.file_exists(highscore_file_path):
+		highscores = []
+		return
+
+	var file = FileAccess.open(highscore_file_path, FileAccess.READ)
+	if file:
+		var json_string = file.get_as_text()
+		file.close()
+		
+		var json = JSON.new()
+		var error = json.parse(json_string)
+		if error == OK:
+			highscores = json.data
+			print("Highscores geladen: ", highscores)
+		else:
+			print("JSON Fehler beim Laden")
